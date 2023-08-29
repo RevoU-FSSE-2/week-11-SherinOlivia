0ff1c22adae7a01ddfac7e01e408bff35807d734
Sherin Olivia <SOChronicle@gmail.com>
2023-08-30 06:39:47 +0700

    feat: create new product, update product qty & price, get all product, get product by name

---
+++ src/controller/productsController.ts
@@ -0,0 +1,100 @@
+import { Request, Response } from 'express';
+import { DBLocal } from '../config/dbConnection';
+import { errorHandling } from './errorHandling';
+import bcrypt from 'bcrypt'
+import jwt, { Secret } from 'jsonwebtoken'
+import JWT_TOKEN from '../config/jwtConfig'
+import { RowDataPacket } from 'mysql2';
+
+// Create new Product
+const createNewProduct = async (req: Request, res: Response) => {
+    try {
+        const { name, qty, price } = req.body
+        const [existingProduct] = await DBLocal.promise().query(`SELECT * FROM week11Milestone2.products WHERE name = ?`, [name]) as RowDataPacket[];
+        if (existingProduct.length === 0) {
+            await DBLocal.promise().query(
+            `INSERT INTO week11Milestone2.products (name, qty, price) VALUES (?, ?, ?)`,
+            [name, qty, price]);
+
+            const [newProduct] = await DBLocal.promise().query(`SELECT * FROM week11Milestone2.products WHERE name = ?`, [name]) as RowDataPacket[];
+            res.status(200).json(errorHandling(newProduct, null));
+        } else {
+            res.status(400).json(errorHandling(null, "Product already exist...!!"));
+            return
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json(errorHandling(null, "Can't create new product...!! Internal Error!"));
+    }
+}
+
+// Update Product Qty & Price
+
+ const updateProduct = async (req: Request, res: Response) => {
+    try {
+        const name = req.params.name
+        const { qty, price } = req.body
+        const [existingProduct] = await DBLocal.promise().query(`SELECT * FROM week11Milestone2.products WHERE name = ?`, [name]) as RowDataPacket[];
+        
+        if (existingProduct.length === 0) {
+            res.status(400).json(errorHandling(null, "Product doesn't exist...!!"));
+            return
+
+        } else {
+            await DBLocal.promise().query(
+            `UPDATE week11Milestone2.products SET qty = ?, price = ? WHERE name = ?`,
+            [qty, price, name]);
+            res.status(200).json(errorHandling(existingProduct, null));
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json(errorHandling(null, "Can't Update Product...!! Internal Error!"));
+    }
+ }
+
+//  get all Product Data
+
+ const getAllProduct = async (req: Request, res: Response) => {
+    try {
+        const getAllProduct = await DBLocal.promise().query(`SELECT * FROM week11Milestone2.products`) as RowDataPacket[];
+        
+        if (getAllProduct.length === 0) {
+            res.status(400).json(errorHandling(null, "Product doesn't exist...!!"));
+            return
+
+        } else {
+            res.status(200).json(errorHandling(getAllProduct[0], null));
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json(errorHandling(null, "Can't Get All Product Data...!! Internal Error!"));
+    }
+ }
+
+// get product by name
+
+const getOneProductName = async (req: Request, res: Response) => {
+    try {
+        const name = req.params.name
+
+        const [getOneProduct] = await DBLocal.promise().query(`SELECT * FROM week11Milestone2.products WHERE name = ?`, [name]) as RowDataPacket[];
+        if (getOneProduct.length === 0) {
+            res.status(400).json(errorHandling(null, "Product doesn't exist...!!"));
+            return
+        } else {
+            res.status(200).json(errorHandling(getOneProduct, null));
+        }
+    } catch (error) {
+        console.error(error)
+        res.status(500).json(errorHandling(null, "Can't Product Data...!! Internal Error!"));
+    }
+}
+
+// delete product
+
+
+
+
+const productsController = {createNewProduct, updateProduct, getAllProduct, getOneProductName}
+
+export default productsController
--- src/router/productsRouter.ts
+++ src/router/productsRouter.ts
@@ -1,25 +1,25 @@
 import express from 'express'
 const productrouter = express.Router()
-// import productsController from '../controller/productsController';
+import productsController from '../controller/productsController';
 import authenMiddleware from '../middleware/authenticationMiddleware'
 import authorMiddleware from '../middleware/authorizationMiddleware'
 
 // Get All Product Data ===> Open to everyone
-// productrouter.get('/', productsController.getAllProduct);
+productrouter.get('/', productsController.getAllProduct);
 
 // Get One Product Data by id
 // productrouter.get('/:id', productsController.getOneProductId);
 
 // Get One Product Data by name
-// productrouter.get('/:name', productsController.getOneProductName);
+productrouter.get('/:name', productsController.getOneProductName);
 
 // Create / Add new Product ===> Staff & Admin Only!
-// productrouter.post('/new',authenMiddleware, authorMiddleware({ roles: ['staff', 'admin'] }), productsController.createNewProduct);
+productrouter.post('/new',authenMiddleware, authorMiddleware(['staff','admin']), productsController.createNewProduct);
 
 // Update Product Qty / Price ===> Staff & Admin Only!
-// productrouter.patch('/update/:id',authenMiddleware, authorMiddleware({ roles: ['staff', 'admin'] }), productsController.updateProduct);
+productrouter.patch('/update/:name',authenMiddleware, authorMiddleware(['staff','admin']), productsController.updateProduct);
 
 // "Delete" Product == isDeleted ===> Admin Only!
-// productrouter.delete('/:id',authenMiddleware, authorMiddleware({ roles: ['admin'] }), productsController.deleteProduct);
+// productrouter.delete('/:id',authenMiddleware, authorMiddleware(['admin']), productsController.deleteProduct);
 
-// export default productrouter
+export default productrouter
