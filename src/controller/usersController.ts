2c4989054f0b2863fa1eb5e4971376008697ad35
Sherin Olivia <SOChronicle@gmail.com>
2023-08-30 06:38:49 +0700

    chore: debugged (functions working 98%)

--- src/controller/usersController.ts
+++ src/controller/usersController.ts
@@ -1,47 +1,55 @@
 import { Request, Response } from 'express';
 import { DBLocal } from '../config/dbConnection';
-import { errorHandling, query } from './errorHandling';
+import { errorHandling } from './errorHandling';
 import bcrypt from 'bcrypt'
 import jwt, { Secret } from 'jsonwebtoken'
 import JWT_TOKEN from '../config/jwtConfig'
-import { RoleReq } from '../type/interface';
+import { RowDataPacket } from 'mysql2';
 
 // Register Account (Reminder: default is cust, admin can register staff)
 const registerUser = async (req: Request, res: Response) => {
     try {
         const { username, password, role } =  req.body;
         const hashedPass = await bcrypt.hash(password, 10)
-        const dbUser: any[] = await DBLocal.promise().query(`INSERT INTO week11Milestone2.users (username, password, role) VALUE (${username}, ${hashedPass})`);
+        const [existingUser] = await DBLocal.promise().query(`SELECT * FROM week11Milestone2.users WHERE username = ?`, [username]) as RowDataPacket[];
         
-        const existedUser = await DBLocal.promise().query(`SELECT * FROM week11Milestone2.users WHERE username = ${username}`);
-        if (existedUser) {
-            res.status(400).json(errorHandling(null, "Username already exist...!!"));
+        if (existingUser.length === 0) {
+            const newUser = await DBLocal.promise().query(
+            `INSERT INTO week11Milestone2.users (username, password, role) VALUES (?, ?, ?)`,
+            [username, hashedPass, 'cust']);
+            res.status(200).json(errorHandling(newUser, null));
         } else {
-            res.status(200).json(errorHandling({ id: dbUser[0].insertId as { id: number }, dbUser}, null));
+            res.status(400).json(errorHandling(null, "Username already exist...!!"));
+            return
         }
-
     } catch (error) {
+        console.error(error)
         res.status(500).json(errorHandling(null, "Register User Failed..!! Internal Error!"));
     }
 }
 
 // Login Account
+
 const loginUser = async (req: Request, res: Response) => {
     try {
         const { username, password } = req.body
-        const existedUser: any = await DBLocal.promise().query(`SELECT * FROM week11Milestone2.users WHERE username = ${username}`);
-        const passwordCheck = await bcrypt.compare(password, existedUser[0].password)
+        const existingUser = await DBLocal.promise().query("SELECT * FROM week11Milestone2.users WHERE username = ?", [username]) as RowDataPacket[];
+        const user = existingUser[0][0]
+        console.log(user, "password:", user.password)
+        
+        const passwordCheck = await bcrypt.compare(password, user.password) 
 
         if (passwordCheck) {
-            const token = jwt.sign({ username: existedUser[0].username, id: existedUser[0].id, role: existedUser[0].role }, JWT_TOKEN as Secret)
+            const token = jwt.sign({ username: user.username, id: user.id, role: user.role }, JWT_TOKEN as Secret)
             res.status(200).json(errorHandling({
-              message: `${existedUser[0].username} Successfully logged in as ${existedUser[0].role}`,
-              data: token}, null))
-        } else {
-            res.status(400).json(errorHandling(null, "Wrong Username or Password...!! Please Try Again..!!"));
-        }
+                message: `${user.username} Successfully logged in as ${user.role}`,
+                data: token}, null))
+          } else {
+            res.status(400).json(errorHandling('Password is incorrect', null))
+          }
     } catch (error) {
-        res.status(500).json(errorHandling(null, "Login Failed..!! Internal Error!"));
+        console.error(error)
+        res.status(500).json(errorHandling('Cannot Connect!! Internal Error!', null));
     }
 }
 
@@ -62,26 +70,28 @@ const getAllUser = async (req: Request, res: Response) => {
 
 // Patch/Update name & address
 
-const updateUser: any = async (req: RoleReq, res: Response) => {
+const updateUser = async (req: Request, res: Response) => {
     try {
-        const { role, username } = req.user;
+        const { role, username } = (req as any).user;
 
         const checkUsername = req.params.username
         const { name, address } = req.body
         if ((role !== "staff" && role !== "admin") && username === checkUsername) {
             const updateData = await DBLocal.promise().query(`
-            UPDATE week11Milestone2.users
-            SET name = ${name}, address = ${address}
-            WHERE username = ${username}`)
+                UPDATE week11Milestone2.users
+                SET name = ?, address = ?
+                WHERE username = ?`,
+                [name, address, username]);
 
             res.status(200).json(errorHandling({
                 message: "User Data Updated Successfully",
                 data: updateData}, null));
         } else if (role == "staff" || role == "admin") {
             const updateData = await DBLocal.promise().query(`
-            UPDATE week11Milestone2.users
-            SET name = ${name}, address = ${address}
-            WHERE username = ${username}`)
+                UPDATE week11Milestone2.users
+                SET name = ?, address = ?
+                WHERE username = ?`,
+                [name, address, username])
 
             res.status(200).json(errorHandling({
                 message: "User Data Updated Successfully",
@@ -89,11 +99,10 @@ const updateUser: any = async (req: RoleReq, res: Response) => {
         }
 
     } catch (error) {
+        console.error(error)
         res.status(500).json(errorHandling(null, "User Data Update Failed...!!"));
     }
 }
-
-
 
 
 const usersController = { registerUser, loginUser, getAllUser, updateUser }
